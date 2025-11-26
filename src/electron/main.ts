1a7c277a952a84cfd778709f6c5a11bc9bc3f8e7
Jürgen Jacobsen <jurgenjacobsen@outlook.com>
2025-11-26 23:04:50 +0000

    🏗️ Base electron code

---
+++ src/electron/main.ts
@@ -0,0 +1,27 @@
+import { app, BrowserWindow, ipcMain } from 'electron';
+import path from 'path';
+import { isDev } from './util.js';
+import { getPreloadPath } from './pathResolver.js';
+import { getStaticData, pollResources } from './resourceManager.js';
+
+app.on('ready', () => {
+    const mainWindow = new BrowserWindow({
+        icon: path.join(app.getAppPath(), '/assets/icon.png'),
+        autoHideMenuBar: true,
+        webPreferences: {
+            preload: getPreloadPath(),
+        }
+    });
+
+    if (isDev()) {
+        mainWindow.loadURL('http://localhost:5123');
+    } else {
+        mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'))
+    }
+
+    pollResources(mainWindow);
+
+    ipcMain.handle('getStaticData', () => {
+        return getStaticData();
+    });
+});
---
+++ src/electron/preload.cts
@@ -0,0 +1,10 @@
+const electron = require('electron');
+
+electron.contextBridge.exposeInMainWorld("electron", {
+    subscribeStatistics: (callback) =>  {
+        electron.ipcRenderer.on('statistics', (_: unknown, stats: any) => {
+            callback(stats);
+        })
+    },
+    getStaticData: () => electron.ipcRenderer.invoke('getStaticData'),
+} satisfies Window['electron']);
