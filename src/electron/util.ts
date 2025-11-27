100af796c7abfeb241f0b8aa76587c69abe31ddf
Jürgen Jacobsen <jurgenjacobsen@outlook.com>
2025-11-27 18:22:28 +0000

    🔥 Removed database integration and related IPC handlers

--- src/electron/util.ts
+++ src/electron/util.ts
@@ -1,51 +1,7 @@
-import { BrowserWindow, ipcMain, WebContents, WebFrameMain, app } from 'electron';
-import { pathToFileURL } from 'url';
-import crypto from 'crypto';
-import { getUIPath } from './pathResolver.js';
+import { BrowserWindow, app } from 'electron';
 
 export function isDev(): boolean {
     return process.env.NODE_ENV === 'development';
-}
-
-export function generateId() {
-    return crypto.randomUUID();
-}
-
-export function ipcMainHandle<Key extends keyof EventPayloadMapping>(
-  key: Key,
-  handler: () => any //EventPayloadMapping[Key]
-) {
-  ipcMain.handle(key, (event) => {
-    validateEventFrame(event.senderFrame);
-    return handler();
-  });
-}
-
-export function ipcMainOn<Key extends keyof EventPayloadMapping>(
-  key: Key,
-  handler: (payload: EventPayloadMapping[Key]) => void
-) {
-  ipcMain.on(key, (event, payload) => {
-    validateEventFrame(event.senderFrame);
-    return handler(payload);
-  });
-}
-
-export function ipcWebContentsSend<Key extends keyof EventPayloadMapping>(
-  key: Key,
-  webContents: WebContents,
-  payload: EventPayloadMapping[Key]
-) {
-  webContents.send(key, payload);
-}
-
-export function validateEventFrame(frame: WebFrameMain | null) {
-  if (isDev() && new URL(frame?.url || '').host === 'localhost:5123') {
-    return;
-  }
-  if (frame?.url !== pathToFileURL(getUIPath()).toString()) {
-    throw new Error('Malicious event');
-  }
 }
 
 export function handleCloseEvents(mainWindow: BrowserWindow) {
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,15 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
-    useEffect(() => {
-        window.electron.subscribeStatistics((stats) => {
-            console.log('Received stats:', stats);
-        });
-    }, [])
 
   return (
     <>
