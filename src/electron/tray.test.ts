c2de8b18a18f17df1fc86bbd2db0a7c15a1537cc
Jürgen Jacobsen <jurgenjacobsen@outlook.com>
2025-12-03 20:46:39 +0000

    ✨ Update tray test to include missing event handler and correct template length

--- src/electron/tray.test.ts
+++ src/electron/tray.test.ts
@@ -6,6 +6,7 @@ vi.mock('electron', () => {
     return {
         Tray: vi.fn().mockImplementation(function(this: any) {
             this.setContextMenu = vi.fn();
+            this.on = vi.fn();
         }),
         app: {
             getAppPath: vi.fn().mockReturnValue('/'),
@@ -28,13 +29,13 @@ test('', () => {
     const calls = (Menu.buildFromTemplate as any as Mock).mock.calls;
     const args = calls[0] as Parameters<typeof Menu.buildFromTemplate>;
     const template = args[0];
-    expect(template).toHaveLength(2);
+    expect(template).toHaveLength(4);
 
-    expect(template[0].label).toEqual('Show');
-    template[0]?.click?.(null as any, null as any, null as any);
+    expect(template[2].label).toEqual('Show');
+    template[2]?.click?.(null as any, null as any, null as any);
     expect(mainWindow.show).toHaveBeenCalled();
     expect(app.dock?.show).toHaveBeenCalled();
 
-    template[1]?.click?.(null as any, null as any, null as any);
+    template[3]?.click?.(null as any, null as any, null as any);
     expect(app.quit).toHaveBeenCalled();
 });
